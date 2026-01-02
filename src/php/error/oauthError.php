bda0d96692ee3358a6ac2edd89e076be00e1024a
hiromushinjo <v.versehub@gmail.com>
2026-01-02 17:37:27 +0900

    fix: スタイルシートのパスを修正

--- src/php/error/oauthError.php
+++ src/php/error/oauthError.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/oauthError.css">
+    <link rel="stylesheet" href="../../css/oauthError.css">
     <title>認証エラー -Vmatch-</title>
 </head>
 
--- src/php/error/systemError.php
+++ src/php/error/systemError.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/systemError.css">
+    <link rel="stylesheet" href="../../css/systemError.css">
     <title>システムエラー -Vmatch-</title>
 </head>
 
