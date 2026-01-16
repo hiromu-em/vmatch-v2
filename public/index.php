9e77bdf526c2e6df24bbee30af86c7d81927c0b8
hiromushinjo <v.versehub@gmail.com>
2026-01-16 15:02:58 +0900

    refactor: ファイルを読み込むパスを修正

--- public/index.php
+++ public/index.php
@@ -2,6 +2,6 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../routers/routers.php';
+require_once __DIR__ . '/../core/routers.php';
 
 $router->dispatch($_SERVER['REQUEST_METHOD'], $_SERVER['REQUEST_URI']);
