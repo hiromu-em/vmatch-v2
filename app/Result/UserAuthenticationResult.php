459e02b26d1b8fbea3a367c87b791f8583c0c87e
hiromushinjo <v.versehub@gmail.com>
2026-01-21 21:42:39 +0900

    refactor: constructorを追加し、メソッドの戻り値を修正

--- app/Result/UserAuthenticationResult.php
+++ app/Result/UserAuthenticationResult.php
@@ -7,15 +7,24 @@ use Vmatch\Result;
 
 class UserAuthenticationResult extends Result
 {
-
-    public static function success()
+    public function __construct(bool $success, array $messages = [])
     {
-
+        $this->success = $success;
+        $this->errors = $messages;
     }
 
-    public static function failure(string $errorMessage)
+    public static function success(): UserAuthenticationResult
     {
-
+        return new self(true);
     }
 
+    public static function failure(array $errorMessages): UserAuthenticationResult
+    {
+        return new self(false, $errorMessages);
+    }
+
+    public function isSuccess(): bool
+    {
+        return $this->success;
+    }
 }
