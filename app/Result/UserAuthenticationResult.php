7007c9965917da4872df76aceb4479f632e5d5e5
hiromushinjo <v.versehub@gmail.com>
2026-01-21 20:49:35 +0900

    add: ユーザー認証に関わるResultを統一のclassとして作成

---
+++ app/Result/UserAuthenticationResult.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace Result;
+
+use Vmatch\Result;
+
+class UserAuthenticationResult extends Result
+{
+
+    public static function success()
+    {
+
+    }
+
+    public static function failure(string $errorMessage)
+    {
+
+    }
+
+}
