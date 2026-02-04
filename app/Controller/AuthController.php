058701bb767020566a81e964a895bc7448ae996a
hiromushinjo <v.versehub@gmail.com>
2026-02-04 19:56:48 +0900

    refactor: 値が存在しない場合は空文字を設定する

--- app/Controller/AuthController.php
+++ app/Controller/AuthController.php
@@ -64,7 +64,7 @@ class AuthController
 
         $verificationTokenResult = $registerService->validateCertificationToken(
             $verificationToken,
-            $handleToken['token']
+            $handleToken['token'] ?? ''
         );
 
         if (!$verificationTokenResult->isSuccess()) {
