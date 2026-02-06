6394f436053d9e982e0d2a5b0545eec935ef1880
hiromushinjo <v.versehub@gmail.com>
2026-02-06 19:40:07 +0900

    refactor: constructを定義

--- app/Controller/UserSettingController.php
+++ app/Controller/UserSettingController.php
@@ -4,9 +4,19 @@ declare(strict_types=1);
 namespace Controller;
 
 use Core\ViewRenderer;
+use Core\Request;
+use Core\Response;
+use Core\Session;
 
 class UserSettingController
 {
+    public function __construct(
+        private Request $request,
+        private Response $response,
+        private Session $session
+    ) {
+    }
+
     public function showInitProfileSettng(ViewRenderer $viewRenderer)
     {
         $viewRenderer->render(
