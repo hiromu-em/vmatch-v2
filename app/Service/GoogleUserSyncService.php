71eb959cf7f1316644deee17b774a25842cdb30b
hiromushinjo <v.versehub@gmail.com>
2026-03-02 19:54:24 +0900

    add: DBに存在するUserRecordを取得するメソッドを呼び出す

--- app/Service/GoogleUserSyncService.php
+++ app/Service/GoogleUserSyncService.php
@@ -16,6 +16,16 @@ class GoogleUserSyncService
     public function synchronizeUserData(string $providerId, string $email): User
     {
         if ($this->authRepository->providerIdExists($providerId)) {
+
+            $userRecord = $this->authRepository->findUserRecord($email);
+
+            return new User(
+                userId: $userRecord['id'],
+                email: $userRecord['email'],
+                isNewUser: false,
+                providerId: $providerId,
+                providerName: 'Google'
+            );
         }
 
         $userRecord = $this->authRepository->fetchNewUserRecord($email);
