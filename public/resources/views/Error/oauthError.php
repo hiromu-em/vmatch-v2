aded05200636dafd3b1abf3433569a53b1e949f0
hiromushinjo <v.versehub@gmail.com>
2026-03-10 19:44:22 +0900

    change: アプリのタイトルを変更

--- public/resources/views/Error/oauthError.php
+++ public/resources/views/Error/oauthError.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/resources/css/oauthError.css">
-    <title>認証エラー -Vmatch-</title>
+    <title>認証エラー-Vminder-</title>
 </head>
 
 <body>
--- public/resources/views/dashboard.php
+++ public/resources/views/dashboard.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vmatch -ダッシュボード-</title>
+    <title>Vminder-ダッシュボード-</title>
 </head>
 
 <body>
